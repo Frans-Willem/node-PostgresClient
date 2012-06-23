405880ffb1e6184e479aa49a9d1ae1ea8bd6e5e1
Farrin Reid <blakmatrix@gmail.com>
2012-06-23 01:15:29 -0800

    [fix] Changed require('sys') to require('util') for compatibility with node v0.8

--- copytest.js
+++ copytest.js
@@ -1,45 +1,45 @@
 var PostgresClient=require("./lib/PostgresClient").PostgresClient;
 var Config=require("../Config");
-var sys=require("sys");
+var util = require('util');
 var net=require("net");
 
 var client=new PostgresClient(Config.dbInfo);
 client.on("error",function(err) {
-	sys.puts("Error: "+err+" "+err.stack);
+	util.puts("Error: "+err+" "+err.stack);
 });
 client.on("connect",function() {
-	sys.puts("Connect");
+	util.puts("Connect");
 });
 client.on("end",function() {
-	sys.puts("End");
+	util.puts("End");
 });
 
 var bl=client.startBlock();
 
 
 setTimeout(function() {
-	sys.puts("GO!");
+	util.puts("GO!");
 	client.endBlock(bl);
 },1000);
 
 client.simpleQuery("CREATE TABLE testtable (first int4, second int4);",function(err,rows,result) {
 	if (err) {
-		sys.puts("CREATE TABLE Error: "+err);
+		util.puts("CREATE TABLE Error: "+err);
 		return;
 	}
-	sys.puts("CREATE TABLE Callback Arguments:");
-	sys.puts("\trows: "+sys.inspect(rows));
-	sys.puts("\tresult: "+sys.inspect(result));
+	util.puts("CREATE TABLE Callback Arguments:");
+	util.puts("\trows: "+util.inspect(rows));
+	util.puts("\tresult: "+util.inspect(result));
 	client.simpleQuery("INSERT INTO testtable (first,second) VALUES(1,2);INSERT INTO testtable (first,second) VALUES (3,4),(5,6);",function(err,rows1,result1,rows2,result2) {
 		if (err) {
-			sys.puts("INSERT INTO Error: "+err);
+			util.puts("INSERT INTO Error: "+err);
 			return;
 		}
-		sys.puts("INSERT INTO Callback Arguments:");
-		sys.puts("\trows1: "+sys.inspect(rows1));
-		sys.puts("\tresult1: "+sys.inspect(result1));
-		sys.puts("\trows2: "+sys.inspect(rows2));
-		sys.puts("\tresult2: "+sys.inspect(result2));
+		util.puts("INSERT INTO Callback Arguments:");
+		util.puts("\trows1: "+util.inspect(rows1));
+		util.puts("\tresult1: "+util.inspect(result1));
+		util.puts("\trows2: "+util.inspect(rows2));
+		util.puts("\tresult2: "+util.inspect(result2));
 		
 		client.simpleQuery("COPY testtable FROM STDIN DELIMITER '\t';",{
 			copyIn: function(handler) {
@@ -49,38 +49,38 @@ client.simpleQuery("CREATE TABLE testtable (first int4, second int4);",function(
 			},
 			complete: function(err,rows,result) {
 				if (err) {
-					sys.puts("COPY FROM Error: "+err);
+					util.puts("COPY FROM Error: "+err);
 					return;
 				}
-				sys.puts("COPY FROM Callback Arguments:");
-				sys.puts("\trows: "+sys.inspect(rows));
-				sys.puts("\tresult: "+sys.inspect(result));
+				util.puts("COPY FROM Callback Arguments:");
+				util.puts("\trows: "+util.inspect(rows));
+				util.puts("\tresult: "+util.inspect(result));
 				client.simpleQuery("COPY testtable TO STDOUT DELIMITER '\t';",{
 				copyOut:function(format,columns) {
-					sys.puts("CopyOut: "+format);
+					util.puts("CopyOut: "+format);
 				},
 				copyData:function(data) {
-					sys.puts("CopyData: "+data.toString('utf8'));
+					util.puts("CopyData: "+data.toString('utf8'));
 				},
 				copyDone:function() {
-					sys.puts("CopyDone");
+					util.puts("CopyDone");
 				},
 				complete: function(err,rows,result) {
 					if (err) {
-						sys.puts("COPY Error: "+err);
+						util.puts("COPY Error: "+err);
 						return;
 					}
-					sys.puts("COPY Callback Arguments:");
-					sys.puts("\trows: "+sys.inspect(rows));
-					sys.puts("\tresult: "+sys.inspect(result));
+					util.puts("COPY Callback Arguments:");
+					util.puts("\trows: "+util.inspect(rows));
+					util.puts("\tresult: "+util.inspect(result));
 					client.simpleQuery("DROP TABLE testtable;",function(err,rows,result) {
 						if (err) {
-							sys.puts("DROP TABLE Error: "+err);
+							util.puts("DROP TABLE Error: "+err);
 							return;
 						}
-						sys.puts("DROP TABLE Callback Arguments:");
-						sys.puts("\trows: "+sys.inspect(rows));
-						sys.puts("\tresult: "+sys.inspect(result));
+						util.puts("DROP TABLE Callback Arguments:");
+						util.puts("\trows: "+util.inspect(rows));
+						util.puts("\tresult: "+util.inspect(result));
 					});
 				}
 			});
--- test.js
+++ test.js
@@ -1,49 +1,49 @@
 var PostgresClient=require("./lib/PostgresClient").PostgresClient;
 var Config=require("../Config");
-var sys=require("sys");
+var util = require('util');
 var net=require("net");
 
 var client=new PostgresClient(Config.dbInfo);
 client.on("error",function(err) {
-	sys.puts("Error: "+err+" "+err.stack);
+	util.puts("Error: "+err+" "+err.stack);
 });
 client.on("connect",function() {
-	sys.puts("Connect");
+	util.puts("Connect");
 });
 client.on("end",function() {
-	sys.puts("End");
+	util.puts("End");
 });
 
 //Simple queries
 client.simpleQuery("SELECT 1,2,3",function(err,rows,result) {
 	if (err) {
-		sys.puts("Simple query failed: "+err);
+		util.puts("Simple query failed: "+err);
 		return;
 	}
-	sys.puts("Simple query results: "+sys.inspect(rows)+", "+result);
+	util.puts("Simple query results: "+util.inspect(rows)+", "+result);
 });
 //Parametrized queries
 client.extendedQuery("SELECT int4($1) as first, int4($2) as second, int4($3) as third",[4,5,6],function(err,rows,result) {
 	if (err) {
-		sys.puts("Extended query failed: "+err);
+		util.puts("Extended query failed: "+err);
 		return;
 	}
-	sys.puts("Extended query results: "+sys.inspect(rows)+", "+result);
+	util.puts("Extended query results: "+util.inspect(rows)+", "+result);
 });
 //Prepared statements extended
 client.parse("SELECT $1, $2, $3",function(err,statementName) {
 	if (err) {
-		sys.puts("Unable to parse prepared statement");
+		util.puts("Unable to parse prepared statement");
 		return;
 	}
 	client.bindExecute(statementName,[7,8,9],function(err,rows,result) {
 		//Clean up the statement after we've used it.
 		client.closeStatement(statementName,function() {});
 		if (err) {
-			sys.puts("Unable to execute statement: "+err);
+			util.puts("Unable to execute statement: "+err);
 			return;
 		}
-		sys.puts("Extended prepared query results: "+sys.inspect(rows)+", "+result);
+		util.puts("Extended prepared query results: "+util.inspect(rows)+", "+result);
 	});
 });
 
@@ -54,37 +54,37 @@ preparedStatement.execute([10,11,12],function(err,rows,result) {
 	//Close it, we're no longer using it.
 	preparedStatement.close(function() {});
 	if (err) {
-		sys.puts("Unable to execute prepared statement: "+err);
+		util.puts("Unable to execute prepared statement: "+err);
 		return;
 	}
-	sys.puts("Simple prepared query results: "+sys.inspect(rows)+", "+result);
+	util.puts("Simple prepared query results: "+util.inspect(rows)+", "+result);
 });
 
 //LISTEN & NOTIFY
 client.simpleQuery("LISTEN seashell;",function(err,rows,result) {
 	if (err) {
-		sys.puts("Unable to LISTEN");
+		util.puts("Unable to LISTEN");
 	} else {
 		//Not sending a payload as only Postgres 9.0+ support it.
 		client.simpleQuery("NOTIFY seashell;",function(err,rows,result) {
 			if (err) {
-				sys.puts("Can't NOTIFY");
+				util.puts("Can't NOTIFY");
 			} else {
-				sys.puts("We should get a notification")
+				util.puts("We should get a notification")
 			}
 		});
 	}
 });
 
 client.on("notification",function(processid,name,payload) {
-	sys.puts("Got a notification!");
-	sys.puts("\tname: "+name);
-	sys.puts("\tpayload: "+payload);
+	util.puts("Got a notification!");
+	util.puts("\tname: "+name);
+	util.puts("\tpayload: "+payload);
 });
 
 client.on("notice",function(notice) {
-	sys.puts("Got a notice:");
-	sys.puts(sys.inspect(notice));
+	util.puts("Got a notice:");
+	util.puts(util.inspect(notice));
 	//Example:
 	//  {"Message":"Hello world","Severity":"NOTICE"}
 	//See http://developer.postgresql.org/pgdocs/postgres/protocol-error-fields.html for all fields
@@ -93,39 +93,39 @@ client.on("notice",function(notice) {
 //CREATE/DELETE/INSERT/DROP support
 client.simpleQuery("CREATE TABLE testtable (first int4, second int4);",function(err,rows,result) {
 	if (err) {
-		sys.puts("CREATE TABLE Error: "+err);
+		util.puts("CREATE TABLE Error: "+err);
 		return;
 	}
-	sys.puts("CREATE TABLE Callback Arguments:");
-	sys.puts("\trows: "+sys.inspect(rows));
-	sys.puts("\tresult: "+sys.inspect(result));
+	util.puts("CREATE TABLE Callback Arguments:");
+	util.puts("\trows: "+util.inspect(rows));
+	util.puts("\tresult: "+util.inspect(result));
 	client.simpleQuery("INSERT INTO testtable (first,second) VALUES(1,2);INSERT INTO testtable (first,second) VALUES (3,4),(5,6);",function(err,rows1,result1,rows2,result2) {
 		if (err) {
-			sys.puts("INSERT INTO Error: "+err);
+			util.puts("INSERT INTO Error: "+err);
 			return;
 		}
-		sys.puts("INSERT INTO Callback Arguments:");
-		sys.puts("\trows1: "+sys.inspect(rows1));
-		sys.puts("\tresult1: "+sys.inspect(result1));
-		sys.puts("\trows2: "+sys.inspect(rows2));
-		sys.puts("\tresult2: "+sys.inspect(result2));
+		util.puts("INSERT INTO Callback Arguments:");
+		util.puts("\trows1: "+util.inspect(rows1));
+		util.puts("\tresult1: "+util.inspect(result1));
+		util.puts("\trows2: "+util.inspect(rows2));
+		util.puts("\tresult2: "+util.inspect(result2));
 		
 		client.simpleQuery("SELECT * FROM testtable;",function(err,rows,result) {
 			if (err) {
-				sys.puts("SELECT Error: "+err);
+				util.puts("SELECT Error: "+err);
 				return;
 			}
-			sys.puts("SELECT Callback Arguments:");
-			sys.puts("\trows: "+sys.inspect(rows));
-			sys.puts("\tresult: "+sys.inspect(result));
+			util.puts("SELECT Callback Arguments:");
+			util.puts("\trows: "+util.inspect(rows));
+			util.puts("\tresult: "+util.inspect(result));
 			client.simpleQuery("DROP TABLE testtable;",function(err,rows,result) {
 				if (err) {
-					sys.puts("DROP TABLE Error: "+err);
+					util.puts("DROP TABLE Error: "+err);
 					return;
 				}
-				sys.puts("DROP TABLE Callback Arguments:");
-				sys.puts("\trows: "+sys.inspect(rows));
-				sys.puts("\tresult: "+sys.inspect(result));
+				util.puts("DROP TABLE Callback Arguments:");
+				util.puts("\trows: "+util.inspect(rows));
+				util.puts("\tresult: "+util.inspect(result));
 			});
 		});
 	});
